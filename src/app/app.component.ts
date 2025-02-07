d1415596e64a48789564783bd330c9cefd0c6bdb
Alex29ec <alejandroestebancantero@gmail.com>
2025-02-07 14:55:29 +0100

    zxczxc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,20 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
-import { AppHttpModule } from './app-http.module';
 import { CatalogoComponent } from './components/catalogo/catalogo.component';
-
+import { ProductosService } from './productos.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AppHttpModule,HeaderComponent, FooterComponent,RouterModule,CatalogoComponent], // Asegúrate de incluir CommonModule
+  imports: [ HeaderComponent, FooterComponent,RouterModule,CatalogoComponent], // Asegúrate de incluir CommonModule
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 
 })
 export class AppComponent {
   title = 'ProyectoAngular';
+  constructor(private productosService: ProductosService) {
+    // Puedes usar el servicio aquí
+  }
 }
